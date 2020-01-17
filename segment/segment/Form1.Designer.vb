4ced99ddad4af678cb27c3fbfd950268030c8e72
Ely Rodriguez <58395324+elyyqttt@users.noreply.github.com>
2020-01-18 02:29:46 +0800

    Add files via upload

---
+++ segment/segment/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.bg = New System.Windows.Forms.PictureBox()
+        Me.pb3 = New System.Windows.Forms.PictureBox()
+        Me.pb5 = New System.Windows.Forms.PictureBox()
+        Me.pb6 = New System.Windows.Forms.PictureBox()
+        Me.pb4 = New System.Windows.Forms.PictureBox()
+        Me.pb7 = New System.Windows.Forms.PictureBox()
+        Me.pb2 = New System.Windows.Forms.PictureBox()
+        Me.pb1 = New System.Windows.Forms.PictureBox()
+        Me.tb = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'bg
+        '
+        Me.bg.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.bg.Location = New System.Drawing.Point(1, -1)
+        Me.bg.Name = "bg"
+        Me.bg.Size = New System.Drawing.Size(625, 582)
+        Me.bg.TabIndex = 7
+        Me.bg.TabStop = False
+        '
+        'pb3
+        '
+        Me.pb3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb3.Location = New System.Drawing.Point(423, 269)
+        Me.pb3.Name = "pb3"
+        Me.pb3.Size = New System.Drawing.Size(28, 143)
+        Me.pb3.TabIndex = 14
+        Me.pb3.TabStop = False
+        '
+        'pb5
+        '
+        Me.pb5.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb5.Location = New System.Drawing.Point(154, 269)
+        Me.pb5.Name = "pb5"
+        Me.pb5.Size = New System.Drawing.Size(28, 143)
+        Me.pb5.TabIndex = 13
+        Me.pb5.TabStop = False
+        '
+        'pb6
+        '
+        Me.pb6.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb6.Location = New System.Drawing.Point(154, 92)
+        Me.pb6.Name = "pb6"
+        Me.pb6.Size = New System.Drawing.Size(28, 143)
+        Me.pb6.TabIndex = 12
+        Me.pb6.TabStop = False
+        '
+        'pb4
+        '
+        Me.pb4.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb4.Location = New System.Drawing.Point(180, 418)
+        Me.pb4.Name = "pb4"
+        Me.pb4.Size = New System.Drawing.Size(242, 22)
+        Me.pb4.TabIndex = 11
+        Me.pb4.TabStop = False
+        '
+        'pb7
+        '
+        Me.pb7.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb7.Location = New System.Drawing.Point(180, 241)
+        Me.pb7.Name = "pb7"
+        Me.pb7.Size = New System.Drawing.Size(242, 22)
+        Me.pb7.TabIndex = 10
+        Me.pb7.TabStop = False
+        '
+        'pb2
+        '
+        Me.pb2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb2.Location = New System.Drawing.Point(423, 92)
+        Me.pb2.Name = "pb2"
+        Me.pb2.Size = New System.Drawing.Size(28, 143)
+        Me.pb2.TabIndex = 9
+        Me.pb2.TabStop = False
+        '
+        'pb1
+        '
+        Me.pb1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.pb1.Location = New System.Drawing.Point(180, 64)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(242, 22)
+        Me.pb1.TabIndex = 8
+        Me.pb1.TabStop = False
+        '
+        'tb
+        '
+        Me.tb.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.tb.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tb.Enabled = False
+        Me.tb.Font = New System.Drawing.Font("Microsoft Uighur", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.tb.Location = New System.Drawing.Point(12, 12)
+        Me.tb.Name = "tb"
+        Me.tb.Size = New System.Drawing.Size(100, 12)
+        Me.tb.TabIndex = 1
+        Me.tb.Text = "1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label1.Font = New System.Drawing.Font("Titillium Web", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label1.Location = New System.Drawing.Point(57, 469)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(522, 73)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Elyzha Lois A. Rodriguez"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(626, 581)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb)
+        Me.Controls.Add(Me.pb3)
+        Me.Controls.Add(Me.pb5)
+        Me.Controls.Add(Me.pb6)
+        Me.Controls.Add(Me.pb4)
+        Me.Controls.Add(Me.pb7)
+        Me.Controls.Add(Me.pb2)
+        Me.Controls.Add(Me.pb1)
+        Me.Controls.Add(Me.bg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents bg As PictureBox
+    Friend WithEvents pb3 As PictureBox
+    Friend WithEvents pb5 As PictureBox
+    Friend WithEvents pb6 As PictureBox
+    Friend WithEvents pb4 As PictureBox
+    Friend WithEvents pb7 As PictureBox
+    Friend WithEvents pb2 As PictureBox
+    Friend WithEvents pb1 As PictureBox
+    Friend WithEvents tb As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ segment/segment/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        tb.Text = tb.Text + 1
+
+        If tb.Text = 2 Then
+            pb1.BackColor = Color.Black
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.Black
+            pb5.BackColor = Color.Black
+            pb6.BackColor = Color.Black
+            pb7.BackColor = Color.Black
+
+        ElseIf tb.Text = 3 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.Black
+            pb7.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.DarkRed
+            pb6.BackColor = Color.Black
+
+        ElseIf tb.Text = 4 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.Black
+            pb6.BackColor = Color.Black
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 5 Then
+            pb1.BackColor = Color.Black
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.Black
+            pb5.BackColor = Color.Black
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 6 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.Black
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.Black
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 7 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.Black
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.DarkRed
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 8 Then
+            pb6.BackColor = Color.DarkRed
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.Black
+            pb5.BackColor = Color.Black
+            pb7.BackColor = Color.Black
+
+        ElseIf tb.Text = 9 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.DarkRed
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 10 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.Black
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.DarkRed
+
+        ElseIf tb.Text = 11 Then
+            pb1.BackColor = Color.DarkRed
+            pb2.BackColor = Color.DarkRed
+            pb3.BackColor = Color.DarkRed
+            pb4.BackColor = Color.DarkRed
+            pb5.BackColor = Color.DarkRed
+            pb6.BackColor = Color.DarkRed
+            pb7.BackColor = Color.Black
+        End If
+
+        Timer1.Interval = 1000
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
